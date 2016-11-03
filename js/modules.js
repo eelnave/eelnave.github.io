babb4c893256403a47640104b7e0c9505568a5de
Evan Peterson <eelnave@gmail.com>
2016-11-03 11:24:25 -0600

    Modularization and Page Improvements

---
+++ js/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+$("#page-header").load("../homework/modules/header.html");
+$("#links").load("../homework/modules/nav.html");
+$("#page-footer").load("../homework/modules/footer.html");
+});
