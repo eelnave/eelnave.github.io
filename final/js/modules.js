4fb4e106bec18dfdce9b2c80f239115600d811c3
Evan Peterson <eelnave@gmail.com>
2016-12-14 17:26:55 -0700

    Almost done

--- final/js/modules.js
+++ final/js/modules.js
@@ -2,4 +2,7 @@ $(function () {
     $("#page-header").load("/final/modules/header.html");
     $("#links").load("/final/modules/nav.html");
     $("#page-footer").load("/final/modules/footer.html");
+    $("#moana").load("/final/modules/moana-info.html");
+    $("#hailcaesar").load("/final/modules/hailcaesar-info.html");
+    $("#sully").load("/final/modules/sully.html");
 });
